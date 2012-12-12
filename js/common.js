aa8cfdc1719d1519e7490b65f659f16133a2ddcf
Artur <morozarturv@gmail.com>
2012-12-12 11:58:22 +0200

    photo popup

--- js/common.js
+++ js/common.js
@@ -7,10 +7,22 @@ $('.codes__scroll').jScrollPane({
 	verticalDragMaxHeight: 11
 });
 
+//gallery scroll
 $('.gallery, .accel__table').jScrollPane({
 	hideFocus: true,
 	contentWidth: '0px',
 	verticalDragMaxHeight: 31
+});
+
+//gallery photo
+$('.gallery a').click(function() {
+	var photo = $(this).attr('href');
+	$('.popup-bg_dark, .popup-photo').fadeIn();
+	$('.popup-photo__pic img').attr('src', photo);
+	return false;
+});
+$('.close-photo').click(function() {
+	$('.popup-bg_dark, .popup-photo').fadeOut();
 });
 
 });
